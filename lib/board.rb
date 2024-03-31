deaa7971c65c6fd5a0dbcc6fe815a632415aa94e
Mateus Matos <98349389+mfm051@users.noreply.github.com>
2024-03-31 17:58:25 -0300

    Add methods to check line, column and diagonals

--- lib/board.rb
+++ lib/board.rb
@@ -6,8 +6,8 @@ class Board
     @columns = Array.new(7) { Array.new(6) }
   end
 
-  def drop_in_column(column_index, symbol)
-    column_to_drop = @columns[column_index]
+  def drop_in_column(col_index, symbol)
+    column_to_drop = @columns[col_index]
 
     column_to_drop.each_with_index do |element, i|
       next unless element.nil?
@@ -19,4 +19,64 @@ class Board
   def column_available?(col_index) = @columns[col_index].any?
 
   def full? = @columns.all?(&:all?)
+
+  def four_in_column?(col_index, symbol)
+    column = @columns[col_index]
+
+    groups_of_four = [column[0..3], column[1..4], column[2..5]]
+
+    groups_of_four.any? { |group| group.all?(symbol) }
+  end
+
+  def four_in_line?(line_index, symbol)
+    line = lines[line_index]
+
+    groups_of_four = [line[0..3], line[1..4], line[2..5]]
+
+    groups_of_four.any? { |group| group.all?(symbol) }
+  end
+
+  def four_in_cross?(col_index, line_index, symbol)
+    crosses = [main_cross(col_index, line_index), anti_cross(col_index, line_index)]
+
+    crosses.each do |cross|
+      next if cross.length < 4
+
+      groups_of_four = [cross[0..3], cross[1..4], cross[2..5]]
+
+      return true if groups_of_four.any? { |group| group.all?(symbol) }
+    end
+
+    false
+  end
+
+  private
+
+  def lines = @columns.transpose
+
+  def main_cross(col_index, line_index)
+    cross = []
+
+    upper_lines = lines[line_index..]
+    lower_lines = lines - upper_lines
+
+    upper_lines.each_with_index { |line, i| cross << line[col_index + i] }
+
+    lower_lines.reverse.each_with_index { |line, i| cross.prepend(line[col_index - i - 1]) }
+
+    cross
+  end
+
+  def anti_cross(col_index, line_index)
+    cross = []
+
+    upper_lines = lines[line_index..]
+    lower_lines = lines - upper_lines
+
+    upper_lines.each_with_index { |line, i| cross.prepend(line[col_index - i]) }
+
+    lower_lines.reverse.each_with_index { |line, i| cross << line[col_index + i + 1] }
+
+    cross
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -75,9 +75,214 @@ describe Board do
       expect(board_full.full?).to eq(true)
     end
 
-    it 'returns false if board is not full' do
+    it 'returns false if board is not completely full' do
+      board_not_full = described_class.new
+      board_not_full.drop_in_column(0, 'X')
+      expect(board_not_full.full?).to eq(false)
+    end
+
+    it 'returns false if board is empty' do
       board_empty = described_class.new
       expect(board_empty.full?).to eq(false)
+    end
+  end
+
+  describe '#four_in_column?' do
+    context 'When there are four equal marks in a column' do
+      subject(:board_four_equal_in_column) { described_class.new }
+
+      before do
+        4.times { board_four_equal_in_column.drop_in_column(0, 'X') }
+      end
+
+      it 'returns true' do
+        col_index = 0
+        symbol = 'X'
+        expect(board_four_equal_in_column.four_in_column?(col_index, symbol)).to eq(true)
+      end
+    end
+
+    context 'When there are four marks in a column that are not equal' do
+      subject(:board_four_different_in_column) { described_class.new }
+
+      before do
+        2.times { board_four_different_in_column.drop_in_column(0, 'X') }
+        2.times { board_four_different_in_column.drop_in_column(0, 'O') }
+      end
+
+      it 'returns false' do
+        col_index = 0
+        symbol = 'X'
+        expect(board_four_different_in_column.four_in_column?(col_index, symbol)).to eq(false)
+      end
+    end
+  end
+
+  describe '#four_in_line?' do
+    context 'when there are four equal marks in a line' do
+      subject(:board_four_equal_in_line) { described_class.new }
+
+      before do
+        board_four_equal_in_line.drop_in_column(0, 'X')
+        board_four_equal_in_line.drop_in_column(1, 'X')
+        board_four_equal_in_line.drop_in_column(2, 'X')
+        board_four_equal_in_line.drop_in_column(3, 'X')
+      end
+
+      it 'returns true' do
+        line_index = 0
+        symbol = 'X'
+        expect(board_four_equal_in_line.four_in_line?(line_index, symbol)).to eq(true)
+      end
+    end
+
+    context 'when there are four marks in a column that are not equal' do
+      subject(:board_four_different_in_line) { described_class.new }
+
+      before do
+        board_four_different_in_line.drop_in_column(0, 'X')
+        board_four_different_in_line.drop_in_column(1, 'X')
+        board_four_different_in_line.drop_in_column(2, 'O')
+        board_four_different_in_line.drop_in_column(3, 'O')
+      end
+
+      it 'returns false' do
+        line_index = 0
+        symbol = 'X'
+        expect(board_four_different_in_line.four_in_line?(line_index, symbol)).to eq(false)
+      end
+    end
+  end
+
+  describe '#four_in_cross?' do
+    context 'when there are four equal marks in the main cross' do
+      subject(:board_four_equal_in_cross) { described_class.new }
+
+      before do
+        board_four_equal_in_cross.drop_in_column(0, 'X')
+
+        board_four_equal_in_cross.drop_in_column(1, 'O')
+        board_four_equal_in_cross.drop_in_column(1, 'X')
+
+        board_four_equal_in_cross.drop_in_column(2, 'O')
+        board_four_equal_in_cross.drop_in_column(2, 'O')
+        board_four_equal_in_cross.drop_in_column(2, 'X')
+
+        board_four_equal_in_cross.drop_in_column(3, 'O')
+        board_four_equal_in_cross.drop_in_column(3, 'O')
+        board_four_equal_in_cross.drop_in_column(3, 'O')
+        board_four_equal_in_cross.drop_in_column(3, 'X')
+      end
+
+      it 'returns true' do
+        col_index = 3
+        line_index = 3
+        symbol = 'X'
+        expect(board_four_equal_in_cross.four_in_cross?(col_index, line_index, symbol)).to eq(true)
+      end
+    end
+
+    context 'when there are four equal marks in the anti cross' do
+      subject(:board_four_equal_in_cross) { described_class.new }
+
+      before do
+        board_four_equal_in_cross.drop_in_column(0, 'O')
+        board_four_equal_in_cross.drop_in_column(0, 'O')
+        board_four_equal_in_cross.drop_in_column(0, 'O')
+        board_four_equal_in_cross.drop_in_column(0, 'X')
+
+        board_four_equal_in_cross.drop_in_column(1, 'O')
+        board_four_equal_in_cross.drop_in_column(1, 'O')
+        board_four_equal_in_cross.drop_in_column(1, 'X')
+
+        board_four_equal_in_cross.drop_in_column(2, 'O')
+        board_four_equal_in_cross.drop_in_column(2, 'X')
+
+        board_four_equal_in_cross.drop_in_column(3, 'X')
+      end
+
+      it 'returns true' do
+        col_index = 3
+        line_index = 0
+        symbol = 'X'
+        expect(board_four_equal_in_cross.four_in_cross?(col_index, line_index, symbol)).to eq(true)
+      end
+    end
+
+    context 'when there are four marks in the main cross that are not equal' do
+      subject(:board_four_different_in_cross) { described_class.new }
+
+      before do
+        board_four_different_in_cross.drop_in_column(0, 'X')
+
+        board_four_different_in_cross.drop_in_column(1, 'O')
+        board_four_different_in_cross.drop_in_column(1, 'X')
+
+        board_four_different_in_cross.drop_in_column(2, 'O')
+        board_four_different_in_cross.drop_in_column(2, 'O')
+        board_four_different_in_cross.drop_in_column(2, 'O')
+
+        board_four_different_in_cross.drop_in_column(3, 'O')
+        board_four_different_in_cross.drop_in_column(3, 'O')
+        board_four_different_in_cross.drop_in_column(3, 'O')
+        board_four_different_in_cross.drop_in_column(3, 'O')
+      end
+
+      it 'returns false' do
+        col_index = 3
+        line_index = 3
+        symbol = 'X'
+        expect(board_four_different_in_cross.four_in_cross?(col_index, line_index, symbol)).to eq(false)
+      end
+    end
+
+    context 'when there are four marks in the anti cross that are not equal' do
+      subject(:board_four_different_in_cross) { described_class.new }
+
+      before do
+        board_four_different_in_cross.drop_in_column(0, 'O')
+        board_four_different_in_cross.drop_in_column(0, 'O')
+        board_four_different_in_cross.drop_in_column(0, 'O')
+        board_four_different_in_cross.drop_in_column(0, 'O')
+
+        board_four_different_in_cross.drop_in_column(1, 'O')
+        board_four_different_in_cross.drop_in_column(1, 'O')
+        board_four_different_in_cross.drop_in_column(1, 'O')
+
+        board_four_different_in_cross.drop_in_column(2, 'O')
+        board_four_different_in_cross.drop_in_column(2, 'X')
+
+        board_four_different_in_cross.drop_in_column(3, 'X')
+      end
+
+      it 'returns false' do
+        col_index = 3
+        line_index = 0
+        symbol = 'X'
+        expect(board_four_different_in_cross.four_in_cross?(col_index, line_index, symbol)).to eq(false)
+      end
+    end
+
+    context 'when cross has less than four slots' do
+      subject(:board_invalid_cross) { described_class.new }
+
+      before do
+        board_invalid_cross.drop_in_column(6, 'O')
+        board_invalid_cross.drop_in_column(6, 'O')
+        board_invalid_cross.drop_in_column(6, 'X')
+
+        board_invalid_cross.drop_in_column(5, 'O')
+        board_invalid_cross.drop_in_column(5, 'X')
+
+        board_invalid_cross.drop_in_column(4, 'X')
+      end
+
+      it 'returns false' do
+        col_index = 4
+        line_index = 0
+        symbol = 'X'
+        expect(board_invalid_cross.four_in_cross?(col_index, line_index, symbol)).to eq(false)
+      end
     end
   end
 end
